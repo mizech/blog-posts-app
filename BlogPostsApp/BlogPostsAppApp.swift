5e30a45b016acbbf7efdadfac0e08ded96be5bd6
mizech <michael.zech@hotmail.de>
2025-01-12 10:49:59 +0100

    Initial Commit

---
+++ BlogPostsApp/BlogPostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogPostsAppApp.swift
+//  BlogPostsApp
+//
+//  Created by Michael on 12.01.25.
+//
+
+import SwiftUI
+
+@main
+struct BlogPostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
